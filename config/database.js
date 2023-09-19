57d47d06759ffb87f6c26cf178e19ebc3c4021a0
Sonam <sonam.brillianceit@gmail.com>
2023-09-19 11:56:28 +0530

    mongo conection updated

--- config/database.js
+++ config/database.js
@@ -5,5 +5,5 @@ main().then((res) => console.log("db connected successfully...!!!"));
 main().catch((err) => console.log(err));
 
 async function main() {
-    mongoose.connect(environmentVariables.MONGODB_CONNECTION);
+    mongoose.connect('mongodb+srv://sonam_brillianceit:HrUek2MQp5I9BsIF@truckextension.b4bjq9c.mongodb.net/truckExtension?retryWrites=true&w=majority');
 }
